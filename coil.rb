f54fc46067c7bf30520a3e18fa9e0213e34a76f0
Katsuhiko Nishimra <ktns.87@gmail.com>
2011-11-17 03:17:23 +0900

    coil.rb: start_angle

--- coil.rb
+++ coil.rb
@@ -5,16 +5,17 @@ require 'complex'
 
 radius = ARGV[0].to_f
 pitch    = ARGV[1].to_f
+start_angle = (ARGV[2] || 0).to_f() *Math::PI / 180
 rotator = Math.exp(Complex::I*pitch * Math::PI / 180)
 
 doc = REXML::Document.new($stdin)
 
 doc.get_elements('molecule/atomArray/atom').collect do |atom|
 	z,y,x = %w<x y z>.collect{|l|atom.attribute(l+'3').to_s.to_f}
-	c = Complex.new(x,y) * rotator
+	c   = Complex.new(x,y) * rotator
 	x,y = c.real, c.imag
-	z += radius
-	c = Complex.polar(z, y / radius)
+	z  += radius
+	c   = Complex.polar(z, y / radius + start_angle)
 	z,y = c.real, c.imag
 	[[z,y,x],%w<x y z>].transpose.each do |v,l|
 		atom.attributes[l+'3'] = '%.6f' % v
