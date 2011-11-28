52c4cc2b01fb51f79fa832f13ebd30f92e791906
Katsuhiko Nishimra <ktns.87@gmail.com>
2011-11-28 17:41:54 +0900

    coil.rb: ruby1.9.2 workaround

--- coil.rb
+++ coil.rb
@@ -1,18 +1,22 @@
 #! /usr/bin/env ruby
 
 require 'rexml/document'
-require 'complex'
+unless Kernel.const_defined? :Complex
+	require 'complex'
+end
 
-radius = ARGV[0].to_f
-pitch    = ARGV[1].to_f
-start_angle = (ARGV[2] || 0).to_f() *Math::PI / 180
-rotator = Math.exp(Complex::I*pitch * Math::PI / 180)
+radius      = ARGV[0].to_f
+pitch       = ARGV[1].to_f
+start_angle = (ARGV[2] || 0).to_f
+$stderr.puts 'radius=%.5f, pitch=%.2f, start_angle=%.2f' % [radius,pitch,start_angle]
+start_angle *= Math::PI / 180
+rotator      = Complex.polar(1, pitch * Math::PI / 180)
 
 doc = REXML::Document.new($stdin)
 
 doc.get_elements('molecule/atomArray/atom').collect do |atom|
 	z,y,x = %w<x y z>.collect{|l|atom.attribute(l+'3').to_s.to_f}
-	c   = Complex.new(x,y) * rotator
+	c   = Complex(x,y) * rotator
 	x,y = c.real, c.imag
 	z  += radius
 	c   = Complex.polar(z, y / radius + start_angle)
