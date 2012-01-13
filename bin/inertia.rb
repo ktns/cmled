b004b1652099a183e215c3d0a0f2572edfa92f2c
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-01-13 19:10:35 +0900

    Moved MassPoint and MassPoints

--- bin/inertia.rb
+++ bin/inertia.rb
@@ -4,55 +4,13 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), *%w<.. lib>)
 require 'rexml/document'
 require 'matrix'
 require 'cmled'
+require 'cmled/masspoint'
 
 include CMLed
 
 MatrixFormat = ((('% 11.3f '*3).rstrip+"\n")*3).freeze
 
 ZeroVector=Vector[0,0,0].freeze
-
-class MassPoint
-	def initialize x,y,z,m
-		@m=m
-		@coords=Vector[x,y,z]
-	end
-
-	attr_reader :m, :coords
-	alias mass m
-
-	def moment center=ZeroVector
-		(@coords - center) * @m
-	end
-
-	def inertia center=ZeroVector
-		m=moment(center)
-		m.inner_product(m)*Matrix.identity(3) -
-			m*m.covector
-	end
-end
-
-class MassPoints
-	def initialize *points
-		if error=points.find{|p| not p.instance_of?(MassPoint)}
-			raise TypeError, 'Expected "%p" but "%p"' % [MassPoint, error.class]
-		end
-		@points=points
-	end
-
-	def center
-		mass,moment=@points.inject([0,ZeroVector]) do |(mass,moment),point|
-			[mass+point.mass, moment+point.moment]
-		end
-		moment*(1/mass)
-	end
-
-	def inertia
-		center=center()
-		@points.inject(Matrix.zero(3)) do |inertia, point|
-			inertia + point.inertia(center)
-		end
-	end
-end
 
 doc = REXML::Document.new($stdin)
 
---
+++ lib/cmled/masspoint.rb
@@ -0,0 +1,44 @@
+module CMLed
+	class MassPoint
+		def initialize x,y,z,m
+			@m=m
+			@coords=Vector[x,y,z]
+		end
+
+		attr_reader :m, :coords
+		alias mass m
+
+		def moment center=ZeroVector
+			(@coords - center) * @m
+		end
+
+		def inertia center=ZeroVector
+			m=moment(center)
+			m.inner_product(m)*Matrix.identity(3) -
+				m*m.covector
+		end
+	end
+
+	class MassPoints
+		def initialize *points
+			if error=points.find{|p| not p.instance_of?(MassPoint)}
+				raise TypeError, 'Expected "%p" but "%p"' % [MassPoint, error.class]
+			end
+			@points=points
+		end
+
+		def center
+			mass,moment=@points.inject([0,ZeroVector]) do |(mass,moment),point|
+			[mass+point.mass, moment+point.moment]
+			end
+			moment*(1/mass)
+		end
+
+		def inertia
+			center=center()
+			@points.inject(Matrix.zero(3)) do |inertia, point|
+				inertia + point.inertia(center)
+			end
+		end
+	end
+end
