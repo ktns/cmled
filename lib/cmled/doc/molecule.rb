4c8a9f9c6a9ae3c41e402db6a712645986e54a68
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-01-18 06:30:27 +0900

    Implemented module AtomAttributes

--- lib/cmled/doc/molecule.rb
+++ lib/cmled/doc/molecule.rb
@@ -8,6 +8,41 @@ module CMLed
 			def initialize element
 				raise TypeError unless element.kind_of?(REXML::Element)
 				@elem = element
+				each_atoms do |atom|
+					atom.attributes.extend AtomAttributes
+				end
+			end
+
+			module AtomAttributes
+				def each_attribute &block
+					if block
+						order = %w<id elementType x3 y3 z3>
+						stack = []
+						super do |attribute|
+							if attribute.name == order.first
+								block.call attribute
+								order.shift
+								until order.empty?
+									stack.size.times do
+										stacked = stack.pop
+										if stacked.name == order.first
+											block.call stacked
+											order.shift
+											break false
+										else
+											stack.unshift stacked
+										end
+									end and break
+								end
+							else
+								stack << attribute
+							end
+						end
+						stack.sort_by{|attribute| attribute.name}.each &block
+					else
+						Enumerator.new(self, :each_attribute)
+					end
+				end
 			end
 
 			module AtomAttributes
