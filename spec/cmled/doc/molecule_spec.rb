827d01d60a3ba0ff81ea531c65ff47782f9a8a53
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-01-21 09:58:15 +0900

    Fix a bug on Molecule#each_atom

--- spec/cmled/doc/molecule_spec.rb
+++ spec/cmled/doc/molecule_spec.rb
@@ -1,2 +1,18 @@
 require File.expand_path(File.join(File.dirname(__FILE__), %w<..>*2, 'spec_helper'))
 
+describe CMLed::Doc::Molecule do
+	before do
+		fixture_open('benzene.cml') do |f|
+			@doc = CMLed::Doc.new(f)
+		end
+		@molecule = @doc.each_molecule.first
+	end
+
+	describe '#each_atom' do
+		context 'without block' do
+			subject{@molecule.each_atom}
+
+			it {should be_kind_of Enumerator}
+		end
+	end
+end
