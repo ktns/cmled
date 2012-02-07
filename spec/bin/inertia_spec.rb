340176ed30132cf008b777a59ad3e4dca992ee7c
Katsuhiko Nishimra <ktns.87@gmail.com>
2012-02-08 00:51:34 +0900

    Eliminate warning from rspec

--- spec/bin/inertia_spec.rb
+++ spec/bin/inertia_spec.rb
@@ -1,12 +1,14 @@
 require File.expand_path(File.join(File.dirname(__FILE__), %w<..>, 'spec_helper'))
 
 describe 'executable file `inertia.rb\'' do
-	@@path = bin_path('inertia.rb')
+	before :all do
+		@path = bin_path('inertia.rb')
+	end
 
 	it 'should not raise error' do
 		fixture_open 'benzene.cml' do |f|
 			lambda do
-				execute_file @@path, f
+				execute_file @path, f
 			end.should_not raise_error
 		end
 	end
